cd2a5734662fb7f9636a51b89226d51380b902a5
shazrin <shazxrin@outlook.com>
2023-05-20 17:07:11 +0800

    Frontend integration with the backend build process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
+import com.github.gradle.node.npm.task.NpmInstallTask
+import com.github.gradle.node.npm.task.NpmTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springdoc.openapi.gradle.plugin.OpenApiGeneratorTask
 
 plugins {
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
+    id("org.openapi.generator") version "6.6.0"
+    id("com.github.node-gradle.node") version "5.0.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -16,16 +22,60 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    developmentOnly("org.springdoc:springdoc-openapi-starter-webflux-ui:2.1.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+}
+
+openApiGenerate {
+    generatorName.set("typescript-axios")
+    inputSpec.set("${buildDir}/openapi.json")
+    outputDir.set("${projectDir}/webapp/src/api")
+}
+
+tasks.openApiGenerate {
+    dependsOn(tasks.withType<OpenApiGeneratorTask>())
+}
+
+tasks.withType<NpmInstallTask> {
+    workingDir.set(file("${projectDir}/webapp"))
+}
+
+tasks.register<NpmTask>("buildWebapp") {
+    workingDir.set(file("${projectDir}/webapp"))
+    npmCommand.set(listOf("run", "build"))
+}
+
+tasks.register<Copy>("copyWebapp") {
+    dependsOn(tasks.named("buildWebapp"))
+    from(file("${projectDir}/webapp/dist"))
+    into(file("${projectDir}/src/main/resources/public"))
+}
+
+tasks.register<Delete>("cleanWebapp") {
+    delete(file("${projectDir}/src/main/resources/public"))
+}
+
+tasks.clean {
+    dependsOn(tasks.named("cleanWebapp"))
+}
+
+tasks.processResources {
+    dependsOn(tasks.named("copyWebapp"))
 }
 
 tasks.withType<KotlinCompile> {
