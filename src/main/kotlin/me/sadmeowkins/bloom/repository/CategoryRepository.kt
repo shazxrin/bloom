f86b2df4f65355c272ffa5fca814aa8d736db059
shazrin <shazxrin@outlook.com>
2023-06-03 20:54:38 +0800

    Remove unused imports

--- src/main/kotlin/me/sadmeowkins/bloom/repository/CategoryRepository.kt
+++ src/main/kotlin/me/sadmeowkins/bloom/repository/CategoryRepository.kt
@@ -2,6 +2,5 @@ package me.sadmeowkins.bloom.repository
 
 import me.sadmeowkins.bloom.model.Category
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
 
 interface CategoryRepository : CoroutineCrudRepository<Category, String>
--- src/main/kotlin/me/sadmeowkins/bloom/repository/TaskRepository.kt
+++ src/main/kotlin/me/sadmeowkins/bloom/repository/TaskRepository.kt
@@ -2,7 +2,6 @@ package me.sadmeowkins.bloom.repository
 
 import me.sadmeowkins.bloom.model.Task
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.stereotype.Repository
 
 interface TaskRepository : CoroutineCrudRepository<Task, String> {
     suspend fun findByEndTimeIsNull(): Task?
