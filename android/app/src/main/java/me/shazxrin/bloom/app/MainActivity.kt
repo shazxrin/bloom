90a2b12ac0a796a9eb5a5fb749b36d0206f64ea3
shazrin <shazxrin@outlook.com>
2023-06-12 22:40:57 +0800

    Initial timer UI

--- android/app/src/main/java/me/shazxrin/bloom/app/MainActivity.kt
+++ android/app/src/main/java/me/shazxrin/bloom/app/MainActivity.kt
@@ -3,19 +3,33 @@ package me.shazxrin.bloom.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.twotone.Pause
+import androidx.compose.material.icons.twotone.Stop
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,42 +45,85 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TimerScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TimerScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /* do something */ },
+                expanded = true,
+                icon = { Icon(Icons.Filled.Add, "New task") },
+                text = { Text(text = "New Task") },
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = { paddingValues ->
+            Timer(
+                modifier = Modifier
+                    .consumeWindowInsets(paddingValues)
+                    .fillMaxSize()
+            )
+        }
+    )
+}
+
+@Composable
+fun Timer(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colorScheme.primary)
+        modifier = modifier
             .padding(12.dp),
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            modifier = Modifier.
-                align(Alignment.CenterHorizontally),
-            text = "Hello $name!",
-            fontSize = 48.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            modifier = Modifier.
-                align(Alignment.CenterHorizontally),
-            text = "from meowkins",
-            textAlign = TextAlign.Center
-        )
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(fontSize = 72.sp, text = "00")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(fontSize = 72.sp, text = "00")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(fontSize = 72.sp, text = "00")
+        }
+        Row(
+            modifier = Modifier,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            FilledTonalIconButton(
+                modifier = Modifier
+                    .size(54.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(imageVector = Icons.TwoTone.Pause, contentDescription = "Pause")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            FilledTonalIconButton(
+                modifier = Modifier
+                    .size(54.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(imageVector = Icons.TwoTone.Stop, contentDescription = "Pause")
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TimerPreview() {
     BloomTheme {
-        Greeting("Android")
+        Timer()
     }
 }
