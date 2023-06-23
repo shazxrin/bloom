387e38ffef27b1e3632da3baaa8fa9973eddc64a
shazrin <shazxrin@outlook.com>
2023-06-23 21:02:42 +0800

    Add ProblemDetail to OpenAPI spec for 404 and 400

--- server/src/main/kotlin/me/shazxrin/bloom/server/advice/RestControllerExceptionHandler.kt
+++ server/src/main/kotlin/me/shazxrin/bloom/server/advice/RestControllerExceptionHandler.kt
@@ -5,18 +5,21 @@ import me.shazxrin.bloom.server.exception.StateException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 class RestControllerExceptionHandler : ResponseEntityExceptionHandler() {
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleNotFoundException(ex: NotFoundException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message ?: "");
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message ?: "")
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(StateException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleStateException(ex: StateException): ProblemDetail {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.message ?: "");
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.message ?: "")
     }
 }
