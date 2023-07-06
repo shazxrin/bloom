15f642ed444d5b959f3428f7324a0bb0911ef9b9
shazrin <shazxrin@outlook.com>
2023-07-07 00:28:00 +0800

    Update TaskNotification model

--- server/src/main/kotlin/me/shazxrin/bloom/server/model/TaskNotification.kt
+++ server/src/main/kotlin/me/shazxrin/bloom/server/model/TaskNotification.kt
@@ -3,11 +3,13 @@ package me.shazxrin.bloom.server.model
 import com.mongodb.lang.NonNull
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document
 class TaskNotification(
     @Id
     val id: String = ObjectId.get().toString(),
 
-    @NonNull
+    @field:NonNull
     val taskId: String
 ) : BaseEntity()
