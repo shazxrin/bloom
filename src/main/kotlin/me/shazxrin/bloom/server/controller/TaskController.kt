a7b01e2971311417a15fa95d4e99e01f1014aecc
shazrin <shazxrin@outlook.com>
2023-07-10 23:52:28 +0800

    Fix typo for REST type

--- src/main/kotlin/me/shazxrin/bloom/server/controller/TaskController.kt
+++ src/main/kotlin/me/shazxrin/bloom/server/controller/TaskController.kt
@@ -8,15 +8,7 @@ import me.shazxrin.bloom.server.service.TaskService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -106,7 +98,7 @@ class TaskController @Autowired constructor(private val taskService: TaskService
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PatchMapping("/{id}")
+    @DeleteMapping("/{id}")
     suspend fun deleteTask(@PathVariable id: String) {
         taskService.deleteTask(id)
     }
