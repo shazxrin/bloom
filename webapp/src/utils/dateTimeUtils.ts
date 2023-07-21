20fe81b4391fdf901850459be6ad2ef56e85f984
shazrin <shazxrin@outlook.com>
2023-07-21 11:13:51 +0800

    Fix duration format when zero

--- webapp/src/utils/dateTimeUtils.ts
+++ webapp/src/utils/dateTimeUtils.ts
@@ -1,9 +1,13 @@
 import {formatDuration, secondsToHours, secondsToMinutes} from "date-fns"
 
 export function formatDurationString(duration: number): string {
-    return formatDuration({
-        hours: secondsToHours(duration),
-        minutes: secondsToMinutes(duration) - (secondsToHours(duration) * 60),
-        seconds: duration - (secondsToMinutes(duration) * 60)
-    })
+    return formatDuration(
+        {
+            hours: secondsToHours(duration),
+            minutes: secondsToMinutes(duration) - (secondsToHours(duration) * 60)
+        },
+        {
+            zero: true
+        }
+    )
 }
