778227f1ceaa789e94f833073186f5269067783e
shazrin <mshazrinbs@gmail.com>
2024-06-10 14:14:12 +0800

    Update dependencies and support Java 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,34 @@
 import com.github.gradle.node.pnpm.task.PnpmTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
-    kotlin("plugin.jpa") version "1.9.22"
-    id("org.springframework.boot") version "3.2.5"
-    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
+
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
+    kotlin("plugin.jpa") version "2.0.0"
+
+    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+
+    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
     id("com.github.node-gradle.node") version "7.0.2"
 }
 
 group = "me.shazxrin"
 version = "2.0"
 
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+        jvmTarget.set(JvmTarget.JVM_22)
+    }
+}
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
 }
 
 repositories {
@@ -75,13 +87,6 @@ tasks.register<PnpmTask>("generateClientApi") {
     dependsOn(tasks.named("generateOpenApiDocs"))
 
     pnpmCommand.set(listOf("run", "generateClientApi"))
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "21"
-    }
 }
 
 tasks.forkedSpringBootRun.configure {
