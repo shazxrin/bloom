c84f35fe413dfaeff2d9be591af2dcf30eaea316
shazrin <shazxrin@outlook.com>
2024-10-27 00:08:09 +0800

    Remove unused responses

--- bloom-webapp/app/utils/responses.server.ts
+++ bloom-webapp/app/utils/responses.server.ts
@@ -8,20 +8,8 @@ export const badRequest = <Data>(data: Data) => {
     return json<Data>(data, 400)
 }
 
-export const notFound = () => {
-    return new Response(null, { status: 404 })
-}
-
 export const methodNotAllowed = () => {
     return new Response(null, { status: 405 })
-}
-
-export const forbidden = () => {
-    return new Response(null, { status: 403 })
-}
-
-export const unauthorized = () => {
-    return new Response(null, { status: 401 })
 }
 
 export const serverError = () => {
